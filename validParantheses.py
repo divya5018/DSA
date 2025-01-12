99d075ea05702232b18bf8a8ba93409bbf1101d6
Sridivya-1508 <sridivya1508@msitprogram.net>
2025-01-12 11:45:08 +0530

    valid parentheses

---
+++ validParantheses.py
@@ -0,0 +1,19 @@
+def isValid( s: str) -> bool:
+        l=[]
+        for i in s:
+            if i == '[' or i=='{' or i=='(':
+                l.append(i)
+            elif len(l)>0 and i=='}'and l[-1]=='{':
+                l.pop()
+            elif len(l)>0 and i==']'and l[-1]=='[':
+                 l.pop()
+            elif len(l)>0 and i==')'and l[-1]=='(':
+                 l.pop()  
+            else:
+                return False
+        if len(l)==0:
+            return True
+        else: return False
+
+k=input().strip()
+print(isValid(k))
